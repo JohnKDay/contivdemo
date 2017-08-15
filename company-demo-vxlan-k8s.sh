080ce5ec0af0bd910f2841fae25b956e6fbead98
John Day <johnkday@cisco.com>
2017-08-15 10:54:04 -0700

    Added self testing of pods

--- company-demo-vxlan-k8s.sh
+++ company-demo-vxlan-k8s.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 # Sample script to perform contiv cli configuration with ACI integration
 
+function WaitPrompt {
+  ConfirmPrompt
+  return
+  set +v
+  echo 
+  echo ---------
+  echo 
+  sleep 14
+  set -v 
+  return
+}
+
 # Function to get user confirmation to proceed
 function ConfirmPrompt {
   set +v
+  echo
   while true; do
   read -p "Ready to proceed(y/n)? " choice
   if [ "$choice" == "y" ]; then
@@ -37,6 +50,7 @@ ConfirmPrompt
 # ------------------- Choose the subnet you like...
 
 netctl net create -t default -e vxlan -s 29.91.0.100-29.91.0.200/24 -g 29.91.0.1 newnet
+netctl net create -t default -e vxlan -s 200.200.200.100-200.200.200.200/24 -g 29.91.0.1 newnet
 
 netctl net ls -t default
 
@@ -52,9 +66,6 @@ netctl group create -t default newnet app
 
 netctl group create -t default -p app2db newnet db
 
-#netctl policy rule-add -t default -d in --protocol tcp --action deny app2db 1
-#netctl policy rule-add -t default -d in --protocol udp --action deny app2db 2
-#netctl policy rule-add -t default -d in --protocol icmp --action deny app2db 3
 netctl policy rule-add -t default --action deny app2db 1
 
 ConfirmPrompt
@@ -79,6 +90,7 @@ kubectl create -f db1.yaml
 
 # ------------------- Checking kubectl until pods are running 
 
+sleep 4
 until kubectl get pods db1 | grep Running
 do
   sleep 1
@@ -86,10 +98,24 @@ done
 
 kubectl get pods -o wide
 
+app1IP=$(kubectl describe pod `app1` | grep IP | sed -E 's/IP:[[:space:]]+//')
+app2IP=$(kubectl describe pod `app2` | grep IP | sed -E 's/IP:[[:space:]]+//')
+db1IP=$(kubectl describe pod `db1` | grep IP | sed -E 's/IP:[[:space:]]+//')
+
+echo "app1 IP address: " $app1IP
+echo "app2 IP address: " $app2IP
+echo "db1 IP address: " $db1IP
+
+
 ConfirmPrompt
 
 # ------------------- Confirm that app1 container CAN ping app2 container
+
+kubectl exec -ti app1 -- ping -w 3 ${app2IP}
+
 # ------------------- Confirm that app1 container CAN NOT ping db1 container
+
+kubectl exec -ti app1 -- ping -w 3 ${db1IP}
 
 ConfirmPrompt
 
@@ -102,12 +128,17 @@ netctl policy rule-add -t default -p 10 -d in --protocol icmp  --from-group app 
 
 # ------------------- Confirm that app1 container CAN ping db1 container
 
+kubectl exec -ti app1 -- ping -w 3 ${db1IP}
+
 ConfirmPrompt
 
 # ------------------- Confirm that port range 6666-6669 is open app1->app2
-# ------------------- `nc -zvnw 3 29.91.0.101 6666-6669`
+
+kubectl exec -ti app1 -- nc -zvnw 3 ${app2IP} 6666-6669
+
 # ------------------- Confirm that port range 6666-6669 is open app1->app2
-# ------------------- `nc -zvnw 3 29.91.0.101 6666-6669`
+
+kubectl exec -ti app1 -- nc -zvnw 3 ${db1IP} 6666-6669
 
 
 ConfirmPrompt
@@ -118,6 +149,8 @@ ConfirmPrompt
 netctl policy rule-add -t default -p 10 -d in --protocol tcp --port 6666 --from-group app  --action allow app2db 11
 
 # ------------------- Confirm that port 6666 is allowed between these Containers
+
+kubectl exec -ti app1 -- nc -zvnw 3 ${db1IP} 6666-6669
 
 ConfirmPrompt
 
